e332d7a4ab240a53ed57252fcc2c88942bc82e42
Manish Kumar <kmanish.031198@gmail.com>
2020-09-30 14:35:05 +0530

    initial commit

--- mkCart/settings.py
+++ mkCart/settings.py
@@ -16,12 +16,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9h_#wy*6)%#ug3-uv@7xlryan5a36rqe^j5a$-i0@fo9szu=%n'
+SECRET_KEY = 'm@n!$#kum@r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
